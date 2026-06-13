6a008edb62802832c1af56514251963c8f949f58
zenhkm <zenhkm@gmail.com>
2026-06-13 16:06:13 +0700

    fix: tambahkan cache busting untuk menghindari masalah caching file js lama pada spa

--- app/views/layout.php
+++ app/views/layout.php
@@ -12,9 +12,9 @@ if (strpos($viewPath, 'dashboard/') !== false) {
     <title><?= isset($title) ? htmlspecialchars($title) : 'Ustadzah AI' ?></title>
     <link rel="icon" type="image/svg+xml" href="favicon.svg">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?= filemtime('css/style.css') ?>">
     <?php if (isset($css)) foreach($css as $file): ?>
-    <link rel="stylesheet" href="<?= htmlspecialchars($file) ?>">
+    <link rel="stylesheet" href="<?= htmlspecialchars($file) ?>?v=<?= filemtime($file) ?>">
     <?php endforeach; ?>
 </head>
 <body>
@@ -23,8 +23,8 @@ if (strpos($viewPath, 'dashboard/') !== false) {
     </div>
     
     <?php if (isset($js)) foreach($js as $file): ?>
-    <script src="<?= htmlspecialchars($file) ?>"></script>
+    <script src="<?= htmlspecialchars($file) ?>?v=<?= filemtime($file) ?>"></script>
     <?php endforeach; ?>
-    <script src="js/spa.js"></script>
+    <script src="js/spa.js?v=<?= filemtime('js/spa.js') ?>"></script>
 </body>
 </html>
