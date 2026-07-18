88c348c8ebcbd27e4a704c7f62aaf2ca57ed3a61
zenhkm <zenhkm@gmail.com>
2026-07-18 21:54:23 +0700

    Deploy combined static production build

--- app/views/nifas.php
+++
@@ -1,6 +0,0 @@
-<div style="padding: 15px; background: #fff; border-bottom: 1px solid #eaeaea;">
-    <a href="index.php" style="color: #d3557d; display: inline-flex; align-items: center; font-size: 16px; font-weight: bold; text-decoration: none;">
-        <i class="fas fa-arrow-left" style="margin-right: 8px;"></i> Kembali ke Beranda
-    </a>
-</div>
-<iframe src="nifas-app.html" style="width: 100%; height: calc(100vh - 60px); border: none; display: block;"></iframe>
